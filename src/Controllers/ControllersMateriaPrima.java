7314bec23892a943732ccecd64165d4a5b766274
Dj Trake Mix <Dj Trake Mix@DESKTOP-4F79V93>
2025-07-28 03:43:52 +0200

    guardando cambios antes de cambiar de rama

---
+++ src/Controllers/ControllersMateriaPrima.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controllers;
+
+/**
+ *
+ * @author Dj Trake Mix
+ */
+public class ControllersMateriaPrima {
+    private String nombre;
+    private String codigo;
+    private String unidad;
+    private String medida;
+    private int cantidad;
+    private String tipo;
+    private String proveedor;
+    private String fechaingreso;
+
+    public ControllersMateriaPrima(String nombre, String codigo, String unidad, String medida, int cantidad, String tipo, String proveedor, String fechaingreso) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.unidad = unidad;
+        this.medida = medida;
+        this.cantidad = cantidad;
+        this.tipo = tipo;
+        this.proveedor = proveedor;
+        this.fechaingreso = fechaingreso;
+    }
+    
+    public Object[] toRow() {
+        return new Object[]{ nombre, codigo, unidad, medida, cantidad, tipo, proveedor, fechaingreso};
+    }
+    
+}
